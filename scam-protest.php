7abdd00341d2c437ccddb3f1e606389d3f171c35
Umair Abbas <umairabbass786@gmail.com>
2022-07-29 17:54:13 +0500

    scam protest api created

---
+++ scam-protest.php
@@ -0,0 +1,40 @@
+<?php
+    //libraries
+    include "include/db.php";
+    include "include/functions.php";
+
+    $error = [];
+    $error['status'] = 'error';
+
+    //declaring variables;
+    const EMAIL = 'email';
+
+    //validating inputs
+    foreach ([EMAIL] as $item) {
+        validateInputs($item);
+    }
+
+    //checking user_id
+    $email = $_POST[EMAIL];
+
+    //getting user wallet
+    $sql = "select * from users where email = '$email'";
+    $r = $conn->query($sql);
+    if(mysqli_num_rows($r) >=1) {
+        $row = mysqli_fetch_assoc($r);
+        $user_id = $row['id'];
+        $sql = "select * from wallets where user_id = '$user_id'";
+        $r = $conn->query($sql);
+        $row = mysqli_fetch_assoc($r);
+        //sending response
+        $success = [];
+        $success['status'] = 'success';
+        $success['response'] = [
+            'balance' => number_format($row['balance'],2)
+        ];
+        echo json_encode($success);
+    }
+    else{
+        $error['message'] = "User Not Found";
+        die(json_encode($error));
+    }
